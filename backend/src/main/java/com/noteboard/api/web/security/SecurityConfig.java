cf8803d80b9d11170c3681e15575097a5b3ff941
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-29 12:07:48 -0500

    Remove unused imports and dependencies

--- backend/src/main/java/com/noteboard/api/web/security/SecurityConfig.java
+++ backend/src/main/java/com/noteboard/api/web/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.noteboard.api.web.security;
 
 
 import com.noteboard.api.config.JwtAuthenticationFilter;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
