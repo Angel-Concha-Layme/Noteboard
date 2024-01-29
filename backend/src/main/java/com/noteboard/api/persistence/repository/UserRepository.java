5648f8840859f50e9a8ead1cc623167796e17ae3
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-29 12:06:22 -0500

    Refactor user repository and authentication service, add demo controller

--- backend/src/main/java/com/noteboard/api/persistence/repository/UserRepository.java
+++ backend/src/main/java/com/noteboard/api/persistence/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package com.noteboard.api.persistence.repository;public class UserRepository {
+package com.noteboard.api.persistence.repository;
+
+
+import com.noteboard.api.persistence.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
--- backend/src/main/java/com/noteboard/api/service/AuthenticationService.java
+++ backend/src/main/java/com/noteboard/api/service/AuthenticationService.java
@@ -1,2 +1,51 @@
-package com.noteboard.api.service;public class AuthenticationService {
+package com.noteboard.api.service;
+
+import com.noteboard.api.config.JwtService;
+import com.noteboard.api.dto.request.AuthenticationRequest;
+import com.noteboard.api.dto.request.RegisterRequest;
+import com.noteboard.api.dto.response.AuthenticationResponse;
+import com.noteboard.api.persistence.entity.Role;
+import com.noteboard.api.persistence.entity.User;
+import com.noteboard.api.persistence.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .name(request.name())
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+        var user = userRepository.findByEmail(request.email())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
 }
