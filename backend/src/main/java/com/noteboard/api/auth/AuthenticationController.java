62c9d6a70bc2baff1f845f10974e1b6b2fd54527
Angel Concha Layme <aconchal@unsa.edu.pe>
2024-01-29 12:05:00 -0500

    Refactor AuthenticationController to include register and authenticate endpoints

--- backend/src/main/java/com/noteboard/api/auth/AuthenticationController.java
+++ backend/src/main/java/com/noteboard/api/auth/AuthenticationController.java
@@ -1,2 +1,34 @@
-package com.noteboard.api.auth;public class AuthenticationController {
+package com.noteboard.api.auth;
+
+
+import com.noteboard.api.dto.request.AuthenticationRequest;
+import com.noteboard.api.dto.request.RegisterRequest;
+import com.noteboard.api.dto.response.AuthenticationResponse;
+import com.noteboard.api.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+
+
 }
